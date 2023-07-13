d37e660623afe270f9ce8c84036503a85f5b117f
mhasin2 <64065263+mhasin2@users.noreply.github.com>
2023-07-13 13:37:26 +0500

    Initial commit.

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -68,258 +68,224 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
         bottomNavigationBar: _bottomNavigationBar(),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 30, right: 103, top: 18),
-              child: Text(
+        body: Padding(
+          padding: const EdgeInsets.only(left: 30, right: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
                 "Welcome, Jessie.",
                 style: Theme.of(context).textTheme.bodyLarge?.copyWith(
                     fontSize: 34,
                     fontWeight: FontWeight.w700,
                     color: Theme.of(context).colorScheme.onBackground),
               ),
-            ),
-            Center(
-              child: Stack(
+              Stack(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      top: 34,
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: const BorderRadius.all(Radius.circular(20)),
                     ),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.primary,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(20)),
-                      ),
-                      height: 125,
-                      width: 354,
+                    height: 125,
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 30, right: 30),
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                top: 30, right: 135, left: 30),
-                            child: Text("Your total asset portfolio",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodySmall
-                                    ?.copyWith(fontSize: 16)),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                left: 30, top: 11, right: 170),
-                            child: Text(
-                              "N203,935",
-                              style: Theme.of(context).textTheme.bodyLarge,
-                            ),
+                          Text("Your total asset portfolio",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall
+                                  ?.copyWith(fontSize: 16)),
+                          Text(
+                            "N203,935",
+                            style: Theme.of(context).textTheme.bodyLarge,
                           )
                         ],
                       ),
                     ),
                   ),
                   Positioned(
-                    left: 219,
-                    top: 97,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => MyAssets()));
-                      },
-                      child: Container(
-                        height: 40,
-                        width: 125,
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.onPrimary,
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(15)),
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Invest now",
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodySmall
-                                ?.copyWith(
-                                  fontSize: 16,
-                                  color: Theme.of(context).colorScheme.primary,
-                                ),
+                    left: 30,
+                    right: 30,
+                    top: 70,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MyAssets()));
+                          },
+                          child: Container(
+                            height: 40,
+                            width: 125,
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).colorScheme.onPrimary,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(15)),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Invest now",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall
+                                    ?.copyWith(
+                                      fontSize: 16,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                      ],
                     ),
                   ),
                 ],
               ),
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.only(top: 40.0, left: 30, right: 163),
-                  child: Text(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
                     "Best Plans",
                     style: Theme.of(context)
                         .textTheme
                         .bodyMedium
                         ?.copyWith(fontWeight: FontWeight.w700),
                   ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(top: 30.0, left: 93, right: 30),
-                  child: Text(
+                  Text(
                     "See All →",
                     style: Theme.of(context).textTheme.bodySmall?.copyWith(
                           color: const Color.fromRGBO(0, 0, 0, 1),
                         ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 20, 20, 0),
-                  child: SizedBox(
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
                     height: 170,
                     width: 134,
                     child: Image.asset("assets/image 4.png"),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 20, 20, 0),
-                  child: SizedBox(
+                  SizedBox(
                     height: 170,
                     width: 134,
                     child: Image.asset("assets/b.png"),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 20, 20, 0),
-                  child: SizedBox(
+                  SizedBox(
                     height: 170,
                     width: 82,
                     child: Image.asset("assets/c.png"),
                   ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 48, 280, 0),
-              child: Text("Investment Guide",
+                ],
+              ),
+              Text("Investment Guide",
                   style: Theme.of(context)
                       .textTheme
                       .bodyMedium
                       ?.copyWith(fontWeight: FontWeight.w700)),
-            ),
-            Column(
-              children: [
-                Stack(
-                  children: [
-                    SizedBox(
-                      height: 92,
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                top: 20, left: 28, right: 246),
-                            child: Text(
-                              "Basic type of investments",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodySmall
-                                  ?.copyWith(
-                                      fontWeight: FontWeight.w700,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .secondary),
-                            ),
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 30, right: 193),
-                            child: Text(
-                              "This is how you set your foot for 2020 Stock market recession. What’s next...",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodySmall
-                                  ?.copyWith(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w400,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .secondary),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Positioned(
-                        left: 399,
-                        top: 8,
-                        right: 34,
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 180),
                         child: SizedBox(
+                          height: 92,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Basic type of investments",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall
+                                    ?.copyWith(
+                                        fontWeight: FontWeight.w700,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary),
+                              ),
+                              Text(
+                                "This is how you set your foot for 2020 Stock market recession. What’s next...",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall
+                                    ?.copyWith(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                          right: 30,
+                          child: Image.asset(
+                            "assets/Ellipse 740.png",
                             height: 61,
                             width: 61,
-                            child: Image.asset("assets/Ellipse 740.png")))
-                  ],
-                ),
-                Stack(
-                  children: [
-                    SizedBox(
-                      height: 92,
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                top: 10, left: 28, right: 216),
-                            child: Text(
-                              "How much can you start wit..",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodySmall
-                                  ?.copyWith(
-                                      fontWeight: FontWeight.w700,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .secondary),
-                            ),
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 30, right: 193),
-                            child: Text(
-                              "What do you like to see? It’s a very different market from 2018. The way...",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodySmall
-                                  ?.copyWith(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w400,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .secondary),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Positioned(
-                        left: 399,
-                        top: 8,
-                        right: 34,
+                          ))
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 180.0),
                         child: SizedBox(
-                            height: 61,
-                            width: 61,
-                            child: Image.asset("assets/Ellipse 740.png")))
-                  ],
-                ),
-              ],
-            )
-          ],
+                          height: 92,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "How much can you start wit..",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall
+                                    ?.copyWith(
+                                        fontWeight: FontWeight.w700,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary),
+                              ),
+                              Text(
+                                "What do you like to see? It’s a very different market from 2018. The way...",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall
+                                    ?.copyWith(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                          right: 30,
+                          child: Image.asset("assets/Ellipse 740 (1).png"))
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
         ));
   }
 }
